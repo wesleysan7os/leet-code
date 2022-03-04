e4746dbe2f39ff02b22bcfc36b40803bbb1393d3
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-03-04 17:45:36 -0300

    two sum problem

--- array/two-sum.js
+++ array/two-sum.js
@@ -1,11 +1,13 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-const twoSum = function (nums, target) {};
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-const twoSum = function (nums, target) {
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+ */
+var twoSum = function (nums, target) {};
+
+var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] == target) {
