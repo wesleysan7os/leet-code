06ac18289f8d680628d6fe83223a644b1db215c0
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-03-05 23:29:05 -0300

    search insert position problem

---
+++ array/search-insert-position.js
@@ -0,0 +1,17 @@
+/*
+  Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+  You must write an algorithm with O(log n) runtime complexity.
+*/
+
+const searchInsert = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i;
+    } else if (nums[i] > target) {
+      return i;
+    }
+  }
+};
+
+console.log(searchInsert([2,3,4,6], 5));
